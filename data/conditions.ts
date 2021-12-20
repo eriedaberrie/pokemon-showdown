a327fcf46901badccd5479873ae30b07e8d22ff1
eriedaberrie <eriedaberrie@gmail.com>
2021-12-20 15:17:29 -0800

    a

--- data/conditions.ts
+++ data/conditions.ts
@@ -639,6 +639,44 @@ export const Conditions: {[k: string]: ConditionData} = {
 			this.add('-weather', 'none');
 		},
 	},
+	sandstorm: {
+		name: 'Thunderstorm',
+		effectType: 'Weather',
+		duration: 5,
+		durationCallback(source, effect) {
+			// if (source?.hasItem('smoothrock')) {
+			// 	return 8;
+			// }
+			return 5;
+		},
+		// This should be applied directly to the stat before any of the other modifiers are chained
+		// So we give it increased priority.
+		onModifySpDPriority: 10,
+		onModifySpD(spd, pokemon) {
+			if (pokemon.hasType('Rock') && this.field.isWeather('sandstorm')) {
+				return this.modify(spd, 1.5);
+			}
+		},
+		onFieldStart(field, source, effect) {
+			if (effect?.effectType === 'Ability') {
+				if (this.gen <= 5) this.effectState.duration = 0;
+				this.add('-weather', 'Thunderstorm', '[from] ability: ' + effect, '[of] ' + source);
+			} else {
+				this.add('-weather', 'Thunderstorm');
+			}
+		},
+		onFieldResidualOrder: 1,
+		onFieldResidual() {
+			this.add('-weather', 'Thunderstorm', '[upkeep]');
+			if (this.field.isWeather('thunderstorm')) this.eachEvent('Weather');
+		},
+		onWeather(target) {
+			this.damage(target.baseMaxhp / 16);
+		},
+		onFieldEnd() {
+			this.add('-weather', 'none');
+		},
+	},
 	hail: {
 		name: 'Hail',
 		effectType: 'Weather',
