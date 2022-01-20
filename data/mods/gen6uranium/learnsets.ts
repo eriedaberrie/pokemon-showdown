5cd8b3475a2c97298276354b05cef91e165eb68a
eriedaberrie <eriedaberrie@gmail.com>
2022-01-20 12:35:32 -0800

    Update learnsets.ts

--- data/mods/gen6uranium/learnsets.ts
+++ data/mods/gen6uranium/learnsets.ts
@@ -151,6 +151,7 @@ export const Learnsets: {[k: string]: ModdedLearnsetData} = {
       uproar: ["6T"],
       waterfall: ["6M"],
       waterpulse: ["6M"],
+      crunch: ["7L39"],
     },
   },
   gyaradosnuclear: {
@@ -212,6 +213,7 @@ export const Learnsets: {[k: string]: ModdedLearnsetData} = {
       waterpulse: ["6M"],
       radioacid: ["6S0"],
       nuclearwaste: ["6S0"],
+      crunch: ["7L39"],
     },
 		eventData: [
       {generation: 6, shiny: 1, moves: ["radioacid", "nuclearwaste"]},
