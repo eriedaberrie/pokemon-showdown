74b44da02b3149ae47bf3fb23e4a43a0e9db6f6c
eriedaberrie <eriedaberrie@gmail.com>
2022-01-30 17:06:24 -0800

    unobtainable moves are unobtainable
    replaced the spore-darkvoid-geomancy duct tape and theyre now finally actually unusable

--- config/formats.ts
+++ config/formats.ts
@@ -1,53 +1,53 @@
 export const Formats: FormatList = [
-  {
-    section: 'Uranium Singles',
-  },
+	{
+		section: 'Uranium Singles',
+	},
 	{
 		name: "[Gen 6] Uranium Random Battle",
 		mod: 'gen6',
 		team: 'random',
 		ruleset: ['Obtainable', 'Sleep Clause Mod', 'HP Percentage Mod', 'Cancel Mod'],
 	},
-  {
-    name: '[Gen 6] Gamma',
-    mod: 'gen6uranium',
-    ruleset: ['Standard', 'Nuclear Clause Mod', 'Soft Baton Pass Clause Mod', 'Uranium Pokedex'],
-    banlist: ['Uber', 'Shadow Tag', 'Belly Drum', 'Spore', 'Dark Void', 'Geomancy'],
-  },
-  {
-    name: '[Gen 6] Beta',
-    mod: 'gen6uranium',
-    ruleset: ['[Gen 6] Gamma'],
-    banlist: ['Gamma', 'Arena Trap'],
-  },
-  {
-    name: '[Gen 6] Alpha',
-    mod: 'gen6uranium',
-    ruleset: ['[Gen 6] Beta'],
-    banlist: ['Beta'],
-  },
-  {
-    name: '[Gen 6] Ubers',
-    mod: 'gen6uranium',
-    ruleset: ['Obtainable', 'Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause', 'Uranium Pokedex'],
-  },
+	{
+		name: '[Gen 6] Gamma',
+		mod: 'gen6uranium',
+		ruleset: ['Standard', 'Nuclear Clause Mod', 'Soft Baton Pass Clause Mod', 'Uranium Pokedex'],
+		banlist: ['Uber', 'Shadow Tag', 'Belly Drum'],
+	},
+	{
+		name: '[Gen 6] Beta',
+		mod: 'gen6uranium',
+		ruleset: ['[Gen 6] Gamma'],
+		banlist: ['Gamma', 'Arena Trap'],
+	},
+	{
+		name: '[Gen 6] Alpha',
+		mod: 'gen6uranium',
+		ruleset: ['[Gen 6] Beta'],
+		banlist: ['Beta'],
+	},
+	{
+		name: '[Gen 6] Ubers',
+		mod: 'gen6uranium',
+		ruleset: ['Obtainable', 'Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause', 'Uranium Pokedex'],
+	},
 	{
 		name: "[Gen 6] NatDex Random Battle",
 		mod: 'gen6',
 		team: 'random',
 		ruleset: ['Obtainable', 'Sleep Clause Mod', 'HP Percentage Mod', 'Cancel Mod'],
 	},
-  {
-    name: '[Gen 6] NatDex OU',
-    mod: 'gen6',
-    ruleset: ['Standard', 'Nuclear Clause Mod', 'Soft Baton Pass Clause Mod'],
-    banlist: ['CanonUber', 'Uber', 'Soul Dew', 'Shadow Tag'],
-  },
-  {
-    name: '[Gen 6] NatDex AG',
-    mod: 'gen6',
-    ruleset: ['Obtainable', 'Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause'],
-  },
+	{
+		name: '[Gen 6] NatDex OU',
+		mod: 'gen6',
+		ruleset: ['Standard', 'Nuclear Clause Mod', 'Soft Baton Pass Clause Mod'],
+		banlist: ['CanonUber', 'Uber', 'Soul Dew', 'Shadow Tag'],
+	},
+	{
+		name: '[Gen 6] NatDex AG',
+		mod: 'gen6',
+		ruleset: ['Obtainable', 'Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause'],
+	},
 	{
 		name: "[Gen 6] Custom Game",
 
@@ -59,24 +59,24 @@ export const Formats: FormatList = [
 		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
 	},
 
-  {
-    section: 'Uranium OMs',
-    column: 2,
-  },
+	{
+		section: 'Uranium OMs',
+		column: 2,
+	},
 	{
 		name: "[Gen 6] Uranium Mix and Mega",
 		desc: `Mega Stones and Primal Orbs can be used on almost any Pok&eacute;mon with no Mega Evolution limit.`,
 
 		mod: 'gen6uraniummixandmega',
 		ruleset: ['Standard', 'Overflow Stat Mod', 'Uranium Pokedex', 'Nuclear Clause Mod', 'Soft Baton Pass Clause Mod'],
-		banlist: ['Shadow Tag', 'Gengarite', 'Electrify', 'Spore', 'Geomancy', 'Belly Drum'],
+		banlist: ['Shadow Tag', 'Gengarite', 'Electrify', 'Belly Drum'],
 		restricted: [],
 		onValidateTeam(team) {
 			const itemTable = new Set<ID>();
 			for (const set of team) {
 				const item = this.dex.items.get(set.item);
 				if (!item.exists) continue;
-        		const itemid = (item.id.startsWith('nuclear') ? item.id.slice(0, 7) : item.id) as ID
+						const itemid = (item.id.startsWith('nuclear') ? item.id.slice(0, 7) : item.id) as ID
 				if (itemTable.has(itemid) && (item.megaStone || item.onPrimal)) {
 					return [
 						`You are limited to one of each Mega Stone and Primal Orb.`,
@@ -122,24 +122,24 @@ export const Formats: FormatList = [
 		},
 	},
 
-  {
-    section: 'Uranium Doubles',
-    column: 2,
-  },
-  {
-    name: '[Gen 6] Doubles Uranium',
-    mod: 'gen6uranium',
-    gameType: 'doubles',
-    ruleset: ['Standard Doubles', 'Uranium Pokedex'],
-    banlist: ['DUber'],
-  },
-  {
-    name: '[Gen 6] NatDex Doubles Ubers',
-    mod: 'gen6',
-    gameType: 'doubles',
-    ruleset: ['Standard Doubles'],
-    banlist: [],
-  },
+	{
+		section: 'Uranium Doubles',
+		column: 2,
+	},
+	{
+		name: '[Gen 6] Doubles Uranium',
+		mod: 'gen6uranium',
+		gameType: 'doubles',
+		ruleset: ['Standard Doubles', 'Uranium Pokedex'],
+		banlist: ['DUber'],
+	},
+	{
+		name: '[Gen 6] NatDex Doubles Ubers',
+		mod: 'gen6',
+		gameType: 'doubles',
+		ruleset: ['Standard Doubles'],
+		banlist: [],
+	},
 	{
 		name: "[Gen 6] Doubles Custom Game",
 
@@ -151,16 +151,16 @@ export const Formats: FormatList = [
 		// no restrictions, for serious (other than team preview)
 		ruleset: ['Team Preview', 'Cancel Mod', 'Max Team Size = 24', 'Max Move Count = 24', 'Max Level = 9999', 'Default Level = 100'],
 	},
-  
-  {
-    section: 'Cursed',
-    column: 2,
-  },
-  {
-    name: '[Gen 8] National Dex',
-    mod: 'gen8',
-    ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod',
-      'Nuclear Clause Mod', 'Soft Baton Pass Clause Mod'],
+	
+	{
+		section: 'Cursed',
+		column: 2,
+	},
+	{
+		name: '[Gen 8] National Dex',
+		mod: 'gen8',
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod',
+			'Nuclear Clause Mod', 'Soft Baton Pass Clause Mod'],
 		banlist: [
 			'Alakazam-Mega', 'Arceus', 'Blastoise-Mega', 'Blaziken-Mega', 'Calyrex-Ice', 'Calyrex-Shadow', 'Cinderace', 'Darkrai', 'Darmanitan-Galar',
 			'Deoxys-Attack', 'Deoxys-Base', 'Deoxys-Speed', 'Dialga', 'Dracovish', 'Dragapult', 'Eternatus', 'Genesect', 'Gengar-Mega', 'Giratina',
@@ -168,10 +168,10 @@ export const Formats: FormatList = [
 			'Lugia', 'Lunala', 'Magearna', 'Marshadow', 'Metagross-Mega', 'Mewtwo', 'Naganadel', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane',
 			'Necrozma-Ultra', 'Palkia', 'Pheromosa', 'Rayquaza', 'Reshiram', 'Salamence-Mega', 'Shaymin-Sky', 'Solgaleo', 'Spectrier', 'Tornadus-Therian',
 			'Urshifu-Base', 'Xerneas', 'Yveltal', 'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Zygarde-Base', 'Zygarde-Complete',
-      'Uber', 'Actan-Nuclear', 'Urayne-Gamma', 'Baitatao', 'Leviathao', 'Krakanao', 'Aotius', 'Mutios',
+			'Uber', 'Actan-Nuclear', 'Urayne-Gamma', 'Baitatao', 'Leviathao', 'Krakanao', 'Aotius', 'Mutios',
 			'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag',
 		],
-  },
+	},
 	{
 		name: "[Gen 8] Custom Game",
 
--- data/mods/gen6uranium/moves.ts
+++ data/mods/gen6uranium/moves.ts
@@ -3,4 +3,428 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		inherit: true,
 		basePower: 120,
 	},
+	steamroller: {
+		inherit: true,
+		noSketch: true,
+	},
+	ragepowder: {
+		inherit: true,
+		noSketch: true,
+	},
+	nightdaze: {
+		inherit: true,
+		noSketch: true,
+	},
+	darkvoid: {
+		inherit: true,
+		noSketch: true,
+	},
+	roaroftime: {
+		inherit: true,
+		noSketch: true,
+	},
+	spacialrend: {
+		inherit: true,
+		noSketch: true,
+	},
+	boltstrike: {
+		inherit: true,
+		noSketch: true,
+	},
+	fusionbolt: {
+		inherit: true,
+		noSketch: true,
+	},
+	jumpkick: {
+		inherit: true,
+		noSketch: true,
+	},
+	sacredsword: {
+		inherit: true,
+		noSketch: true,
+	},
+	secretsword: {
+		inherit: true,
+		noSketch: true,
+	},
+	circlethrow: {
+		inherit: true,
+		noSketch: true,
+	},
+	wakeupslap: {
+		inherit: true,
+		noSketch: true,
+	},
+	vacuumwave: {
+		inherit: true,
+		noSketch: true,
+	},
+	triplekick: {
+		inherit: true,
+		noSketch: true,
+	},
+	vcreate: {
+		inherit: true,
+		noSketch: true,
+	},
+	blueflare: {
+		inherit: true,
+		noSketch: true,
+	},
+	fusionflare: {
+		inherit: true,
+		noSketch: true,
+	},
+	sacredfire: {
+		inherit: true,
+		noSketch: true,
+	},
+	searingshot: {
+		inherit: true,
+		noSketch: true,
+	},
+	fierydance: {
+		inherit: true,
+		noSketch: true,
+	},
+	firepledge: {
+		inherit: true,
+		noSketch: true,
+	},
+	heatcrash: {
+		inherit: true,
+		noSketch: true,
+	},
+	aeroblast: {
+		inherit: true,
+		noSketch: true,
+	},
+	chatter: {
+		inherit: true,
+		noSketch: true,
+	},
+	skydrop: {
+		inherit: true,
+		noSketch: true,
+	},
+	shadowforce: {
+		inherit: true,
+		noSketch: true,
+	},
+	seedflare: {
+		inherit: true,
+		noSketch: true,
+	},
+	hornleech: {
+		inherit: true,
+		noSketch: true,
+	},
+	leaftornado: {
+		inherit: true,
+		noSketch: true,
+	},
+	needlearm: {
+		inherit: true,
+		noSketch: true,
+	},
+	grasspledge: {
+		inherit: true,
+		noSketch: true,
+	},
+	spore: {
+		inherit: true,
+		noSketch: true,
+	},
+	boneclub: {
+		inherit: true,
+		noSketch: true,
+	},
+	bonemerang: {
+		inherit: true,
+		noSketch: true,
+	},
+	iceburn: {
+		inherit: true,
+		noSketch: true,
+	},
+	selfdestruct: {
+		inherit: true,
+		noSketch: true,
+	},
+	megakick: {
+		inherit: true,
+		noSketch: true,
+	},
+	judgment: {
+		inherit: true,
+		noSketch: true,
+	},
+	technoblast: {
+		inherit: true,
+		noSketch: true,
+	},
+	relicsong: {
+		inherit: true,
+		noSketch: true,
+	},
+	dizzypunch: {
+		inherit: true,
+		noSketch: true,
+	},
+	cut: {
+		inherit: true,
+		noSketch: true,
+	},
+	struggle: {
+		inherit: true,
+		noSketch: true,
+	},
+	weatherball: {
+		inherit: true,
+		noSketch: true,
+	},
+	cometpunch: {
+		inherit: true,
+		noSketch: true,
+	},
+	barrage: {
+		inherit: true,
+		noSketch: true,
+	},
+	constrict: {
+		inherit: true,
+		noSketch: true,
+	},
+	crushgrip: {
+		inherit: true,
+		noSketch: true,
+	},
+	wringout: {
+		inherit: true,
+		noSketch: true,
+	},
+	conversion: {
+		inherit: true,
+		noSketch: true,
+	},
+	lovelykiss: {
+		inherit: true,
+		noSketch: true,
+	},
+	milkdrink: {
+		inherit: true,
+		noSketch: true,
+	},
+	mimic: {
+		inherit: true,
+		noSketch: true,
+	},
+	minimize: {
+		inherit: true,
+		noSketch: true,
+	},
+	reflecttype: {
+		inherit: true,
+		noSketch: true,
+	},
+	whirlwind: {
+		inherit: true,
+		noSketch: true,
+	},
+	workup: {
+		inherit: true,
+		noSketch: true,
+	},
+	psychoboost: {
+		inherit: true,
+		noSketch: true,
+	},
+	psystrike: {
+		inherit: true,
+		noSketch: true,
+	},
+	lusterpurge: {
+		inherit: true,
+		noSketch: true,
+	},
+	mistball: {
+		inherit: true,
+		noSketch: true,
+	},
+	heartstamp: {
+		inherit: true,
+		noSketch: true,
+	},
+	allyswitch: {
+		inherit: true,
+		noSketch: true,
+	},
+	guardsplit: {
+		inherit: true,
+		noSketch: true,
+	},
+	healingwish: {
+		inherit: true,
+		noSketch: true,
+	},
+	heartswap: {
+		inherit: true,
+		noSketch: true,
+	},
+	kinesis: {
+		inherit: true,
+		noSketch: true,
+	},
+	lunardance: {
+		inherit: true,
+		noSketch: true,
+	},
+	miracleeye: {
+		inherit: true,
+		noSketch: true,
+	},
+	powersplit: {
+		inherit: true,
+		noSketch: true,
+	},
+	powertrick: {
+		inherit: true,
+		noSketch: true,
+	},
+	geargrind: {
+		inherit: true,
+		noSketch: true,
+	},
+	heavyslam: {
+		inherit: true,
+		noSketch: true,
+	},
+	autotomize: {
+		inherit: true,
+		noSketch: true,
+	},
+	waterspout: {
+		inherit: true,
+		noSketch: true,
+	},
+	waterpledge: {
+		inherit: true,
+		noSketch: true,
+	},
+	clamp: {
+		inherit: true,
+		noSketch: true,
+	},
+	eerieimpulse: {
+		inherit: true,
+		noSketch: true,
+	},
+	electrify: {
+		inherit: true,
+		noSketch: true,
+	},
+	fairylock: {
+		inherit: true,
+		noSketch: true,
+	},
+	fellstinger: {
+		inherit: true,
+		noSketch: true,
+	},
+	flowershield: {
+		inherit: true,
+		noSketch: true,
+	},
+	flyingpress: {
+		inherit: true,
+		noSketch: true,
+	},
+	forestscurse: {
+		inherit: true,
+		noSketch: true,
+	},
+	geomancy: {
+		inherit: true,
+		noSketch: true,
+	},
+	grassyterrain: {
+		inherit: true,
+		noSketch: true,
+	},
+	kingsshield: {
+		inherit: true,
+		noSketch: true,
+	},
+	landswrath: {
+		inherit: true,
+		noSketch: true,
+	},
+	magneticflux: {
+		inherit: true,
+		noSketch: true,
+	},
+	nobleroar: {
+		inherit: true,
+		noSketch: true,
+	},
+	oblivionwing: {
+		inherit: true,
+		noSketch: true,
+	},
+	partingshot: {
+		inherit: true,
+		noSketch: true,
+	},
+	petalblizzard: {
+		inherit: true,
+		noSketch: true,
+	},
+	playnice: {
+		inherit: true,
+		noSketch: true,
+	},
+	poweruppunch: {
+		inherit: true,
+		noSketch: true,
+	},
+	rototiller: {
+		inherit: true,
+		noSketch: true,
+	},
+	spikyshield: {
+		inherit: true,
+		noSketch: true,
+	},
+	topsyturvy: {
+		inherit: true,
+		noSketch: true,
+	},
+	trickortreat: {
+		inherit: true,
+		noSketch: true,
+	},
+	watershuriken: {
+		inherit: true,
+		noSketch: true,
+	},
+	belch: {
+		inherit: true,
+		noSketch: true,
+	},
+	confide: {
+		inherit: true,
+		noSketch: true,
+	},
+	craftyshield: {
+		inherit: true,
+		noSketch: true,
+	},
+	gemstoneglimmer: {
+		inherit: true,
+		noSketch: true,
+	},
+	oceanswrath: {
+		inherit: true,
+		noSketch: true,
+	},
 };
